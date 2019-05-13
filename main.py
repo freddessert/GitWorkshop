645292921249ff23f65faef2e44e8e726c48bb5b
freddessert <fred.des.16@hotmail.com>
2019-05-12 20:06:53 -0400

    Get merge conflict v2

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 # change this value for a different result
 nterms = 10
-print("Git is awesome!!")
+
 # first two terms
 n1 = 0
 n2 = 1
 count = 0
-
+print("Git is awesome!!")
 # check if the number of terms is valid
 if nterms <= 0:
    print("Please enter a positive integer")
